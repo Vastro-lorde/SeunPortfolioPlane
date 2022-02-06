75c2f4f5184d026458ececb742b5accf8a3701de
Spirit <omatsolaseund@gmail.com>
2022-02-06 10:39:20 +0100

    Added javascript audio on eventlistener

---
+++ index.js
@@ -0,0 +1,9 @@
+
+document.body.addEventListener('click', function() {
+    var audio = new Audio("./audio/Monlisa.mp3");
+    audio.play();
+}, {once : true});
+
+document.body.addEventListener('click', function() {
+
+}, {once : true});
